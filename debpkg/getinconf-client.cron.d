bf50720efce270556d6d6e84bd89237415e7393b
Agustí Moll <agusti@biruji.org>
2013-11-27 03:32:20 +0100

    Rename main file. And rename config file.

--- debpkg/getinconf-client.cron.d
+++ debpkg/getinconf-client.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the getinconf-client package
 #
-0 0	* * *	root	[ -x /usr/sbin/stvclient ] && /usr/sbin/stvclient
+0 0	* * *	root	[ -x /usr/sbin/getinconf-client ] && /usr/sbin/getinconf-client
