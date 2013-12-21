2467ab6b61db516e914d110b3b3cb0776d9e96d9
Agustí Moll <agusti@biruji.org>
2013-12-21 09:19:24 +0100

    Fixed bug in cron.d

--- debpkg/getinconf-client.cron.d
+++ debpkg/getinconf-client.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the getinconf-client package
 #
-0 *	* * *	root	[ -x /usr/sbin/getinconf-client ] && "/usr/sbin/getinconf-client install"
+0 *	* * *	root	[ -x /usr/sbin/getinconf-client ] && /usr/sbin/getinconf-client install > /dev/null 2>&1
